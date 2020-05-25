19676d712cfa34416cd7423dd8424040abb756c3
paras <65333213+paras30@users.noreply.github.com>
2020-05-25 22:23:51 +0530

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,10 @@
+from turtle import*
+pensize(3)
+color("blue","yellow")
+for i in range(25):
+         right(139)
+         begin_fill()
+         for i in range (9):
+                 forward(75)
+                 right (45)
+        
